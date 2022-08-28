4bc6056654755b7338d0e73a7a83bed16be527ff
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2022-08-28 13:54:56 -0300

    CHORE: Subindo projeto para repositorio remoto

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller {
+
+    public function create() {
+
+    }
+
+    public function readAll() {
+
+    }
+
+    public function read() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
+Route::prefix('v1/')->group(function() {
+
+    Route::get('ping', function() { 
+        
+        return response()->json(['api_name' => 'apicomlaravel-b7web'], 200);
+
+    });
+
+    Route::post('todo', [TodoController::class, 'create']);
+    Route::get('todos', [TodoController::class, 'readAll']);
+    Route::get('todo/{id}', [TodoController::class, 'read']);
+    Route::put('todo/{id}', [TodoController::class, 'update']);
+    Route::delete('todo/{id}', [TodoController::class, 'delete']);
+
+});
